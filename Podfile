a55ee4582df089d5cbf2279e6224b648956643e0
LightningAntMarket <you@example.com>
2018-02-05 16:25:02 +0800

    commit
    123

--- Podfile
+++
@@ -1,5 +0,0 @@
-platform:ios,'7.0'
-target "KTCacheDemo" do
-pod 'Masonry', '~> 1.0.1'
-pod 'SAMKeychain'
-end
--- Pods/Target Support Files/Pods-KTCacheDemo/Pods-KTCacheDemo-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_KTCacheDemo : NSObject
-@end
-@implementation PodsDummy_Pods_KTCacheDemo
-@end
